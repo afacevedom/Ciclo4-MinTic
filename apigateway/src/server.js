edf66e27b32138debfcfdbd1ef540255cce38cda
DavidK1412 <68391937+DavidK1412@users.noreply.github.com>
2021-12-11 19:49:37 -0500

    Delete apigateway directory

--- apigateway/src/server.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    auth_api_url: 'https://p39g5authms1.herokuapp.com',
-    history_api_url: 'https://group5ms2java.herokuapp.com',
-}
--- apigateway/src/typeDefs/index.js
+++
@@ -1,6 +0,0 @@
-const authTypeDefs = require('./auth_type_defs');
-const historyTypeDefs = require('./history_type_defs');
-
-const schemasArrays = [authTypeDefs, historyTypeDefs];
-
-module.exports = schemasArrays;
